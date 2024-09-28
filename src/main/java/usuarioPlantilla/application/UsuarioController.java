09385c962ed88e9b454428c1ec2633ebe2c4359e
Gabo <gab11vboss@gmail.com>
2024-09-28 15:32:40 -0500

    endpoints agregados

--- src/main/java/usuarioPlantilla/application/UsuarioController.java
+++ src/main/java/usuarioPlantilla/application/UsuarioController.java
@@ -1,13 +1,38 @@
 package usuarioPlantilla.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import usuarioPlantilla.domian.UsuarioPlantilla;
+import usuarioPlantilla.domian.UsuarioService;
 import usuarioPlantilla.infrastructure.UsuarioRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioPlantilla>> getAllUsers() {
+        List<UsuarioPlantilla> usuarios = usuarioService.obtenerUsuarios();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioPlantilla> actualizacionIntegral(@PathVariable long id, @RequestBody UsuarioPlantilla usuario) {
+        UsuarioPlantilla usuarioActualizado = usuarioService.actualizarUsuario(id, usuario);
+        return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<UsuarioPlantilla> actualizarUsuarioParcial(
+            @PathVariable Long id, @RequestBody UsuarioPlantilla usuarioParcial) {
+        UsuarioPlantilla usuario = usuarioService.actualizarUsuarioParcial(id, usuarioParcial);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
 }
--- src/main/java/usuarioPlantilla/domian/UsuarioService.java
+++ src/main/java/usuarioPlantilla/domian/UsuarioService.java
@@ -24,4 +24,34 @@ public class UsuarioService {
         return usuarios;
     }
 
+    public UsuarioPlantilla actualizarUsuario(Long id, UsuarioPlantilla usuarioActualizado) {
+        UsuarioPlantilla usuarioExistente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con el id: " + id));
+
+        usuarioExistente.setNombre(usuarioActualizado.getNombre());
+        usuarioExistente.setApellido(usuarioActualizado.getApellido());
+        usuarioExistente.setEdad(usuarioActualizado.getEdad());
+        usuarioExistente.setTelefono(usuarioActualizado.getTelefono());
+
+        return usuarioRepository.save(usuarioExistente);
+    }
+    public UsuarioPlantilla actualizarUsuarioParcial(Long id, UsuarioPlantilla usuarioParcial) {
+        UsuarioPlantilla usuarioExistente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con el id: " + id));
+
+        if (usuarioParcial.getNombre() != null) {
+            usuarioExistente.setNombre(usuarioParcial.getNombre());
+        }
+        if (usuarioParcial.getApellido() != null) {
+            usuarioExistente.setApellido(usuarioParcial.getApellido());
+        }
+        if (usuarioParcial.getTelefono()!= null) {
+            usuarioExistente.setTelefono(usuarioParcial.getTelefono());
+        }
+        if (usuarioParcial.getEdad()!=0){
+            usuarioExistente.setEdad(usuarioParcial.getEdad());
+        }
+
+        return usuarioRepository.save(usuarioExistente);
+    }
 }
